f00c3991a72b55a45fbdb0bf86da2df08aa5acb2
MuxLee <dnfl7080@naver.com>
2025-08-27 15:58:29 +0900

    [feat]: `Footer` 시맨틱 컴포넌트 추가 - 블로그 페이지에 전반적으로 하단에 사용하기 위해 작성 - 모바일 및 태블릿과 같은 기기를 위한 반응형 디자인 추가

---
+++ src/app/semantic/footer/footer-semantic.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterSemantic } from './footer-semantic.component';
+
+describe('FooterComponent', () => {
+    let component: FooterSemantic;
+    let fixture: ComponentFixture<FooterSemantic>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [FooterSemantic]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(FooterSemantic);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
