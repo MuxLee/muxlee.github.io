1a39cd93bf774a8755eedb60117489effa010039
MuxLee <dnfl7080@naver.com>
2025-10-10 21:27:09 +0900

    [feat]: 빌더 타입 추가 - 빌더 타입 추가 - 빌더 생성자 타입 추가 - 빌더 객체 타입 추가
    *메타데이터 스크립트에서 빌더 패턴을 사용할 수 있도록 타입 정의*

---
+++ script/builder/builder.ts
@@ -0,0 +1,41 @@
+/**
+ * 빌더 인터페이스
+ *
+ * @template R 반환 개체 유형
+ * @author Mux
+ * @version 1.0.0
+ */
+interface Builder<R> {
+
+    /**
+     * 개체를 생성하여 반환합니다.
+     *
+     * @returns {R} 개체
+     */
+    build(): R
+
+}
+
+/**
+ * 빌더 클래스 생성자 유형
+ * 
+ * @template R 반환 개체 유형
+ * @author Mux
+ * @version 1.0.0
+ */
+type BuilderConstructor<R> = Constructor<Builder<R>>;
+
+/**
+ * 빌더 클래스 객체 유형
+ * 
+ * @template R 반환 빌더 클래스 유형
+ * @author Mux
+ * @version 1.0.0
+ */
+type BuilderInstance<R extends BuilderConstructor<R>> = InstanceType<R>;
+
+export type {
+    Builder,
+    BuilderConstructor,
+    BuilderInstance
+};
