5283ee4857533b8c123f0f60afd3a14240c27115
MuxLee <dnfl7080@naver.com>
2025-10-15 15:05:32 +0900

    [feat]: 작업 수행기 기능 추가 - 단위 작업 처리를 위한 `Processor` 인터페이스 추가 - 아무 작업도 처리하지 않는 경우를 대비한 `EmptyConsumerProcessor` 추가

---
+++ script/process/processor.ts
@@ -0,0 +1,46 @@
+/**
+ * 작업 수행기 인터페이스
+ *
+ * @template T 개체 유형
+ * @template R 반환 개체 유형
+ * @author Mux
+ * @version 1.0.0
+ */
+declare interface Processor<T, R> {
+
+    /**
+     * 작업을 수행하여 반환합니다.
+     *
+     * @param {T} object 개체
+     * @returns {R} 개체
+     */
+    process(object: T): R;
+
+}
+
+/**
+ * 빈-작업 수행 클래스
+ * 
+ * @implements {Processor<unknown, void>}
+ * @author Mux
+ * @version 1.0.0
+ */
+class EmptyConsumerProcessor implements Processor<unknown, void> {
+
+    /**
+     * 어떠한 작업도 수행하지 않습니다.
+     * 
+     * @param {unknown} object 개체
+     * @returns {void}
+     */
+    process(object: unknown): void {}
+
+}
+
+export type {
+    Processor
+};
+
+export {
+    EmptyConsumerProcessor
+};
