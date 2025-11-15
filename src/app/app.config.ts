0e0a3b3e9e8d7c3dde99dd51421b5748fd07d955
MuxLee <dnfl7080@naver.com>
2025-11-15 13:58:06 +0900

    [feat]: `Angular` 컴포넌트 라우팅 설정 추가 - 쿼리/매개변수, 경로 및 행렬, 정적, 리졸버 형태의 라우터 바인딩 정보를 가져올 수 있도록 추가

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from '@app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -10,7 +10,7 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideClientHydration(withEventReplay()),
         provideHttpClient(),
-        provideRouter(routes),
+        provideRouter(routes, withComponentInputBinding()),
         provideService(),
         provideZoneChangeDetection({
             eventCoalescing: true
