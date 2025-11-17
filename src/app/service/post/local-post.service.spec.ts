8adaa5a3c57740e2cc2c029bd38aa70fa74592d2
MuxLee <dnfl7080@naver.com>
2025-11-17 14:40:19 +0900

    [fix]: 게시글 조회 서비스 수정 - 지역 게시글 서비스 조회에서 종합 메타데이터가 아닌 게시글 메타데이터를 조회하도록 수정 - 전역 게시글 서비스의 조회 타입을 `Comprehensive`에서 `Post`로 수정

--- src/app/service/post/local-post.service.spec.ts
+++ src/app/service/post/local-post.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { LocalPostService } from './local-post.service';
 
 describe('LocalPostService', () => {
-  let service: LocalPostService;
+    let service: LocalPostService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LocalPostService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(LocalPostService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
