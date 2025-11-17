7adcbb46d8b2d9c0c1b628841fea6a98fdc60a7f
MuxLee <dnfl7080@naver.com>
2025-11-17 14:36:13 +0900

    [fix]: 불필요한 의존성 주입 제거 - `GloblPostService`에 대한 `HttpClient` 의존성 주입 제거

--- src/app/service/post/post.service.ts
+++ src/app/service/post/post.service.ts
@@ -1,14 +1,16 @@
-import { Observable } from 'rxjs';
-import { Comprehensive } from '@model/comprehensive';
-import { registerService } from '@service/service.provider';
-import { GlobalPostService } from '@service/post/global-post.service';
-import { LocalPostService } from '@service/post/local-post.service';
 import { HttpClient } from '@angular/common/http';
 
-abstract class PostService {
+import { type Observable } from 'rxjs';
 
-    getMetadata(): Observable<Comprehensive> {
-        throw new Error('Method not implemented.');
+import { type Post } from '@model/post';
+import { GlobalPostService } from '@service/post/global-post.service';
+import { LocalPostService } from '@service/post/local-post.service';
+import { registerService } from '@service/service.provider';
+
+export default abstract class PostService {
+
+    getPost(postIdentity: string): Observable<Post> {
+        throw new Error(`'getPost' 메소드가 구현되지 않았습니다.`);
     };
 
 }
@@ -16,14 +18,11 @@ abstract class PostService {
 registerService<PostService>({
     environment: 'global',
     token: PostService,
-    useClass: GlobalPostService,
-    deps: [HttpClient]
+    useClass: GlobalPostService
 });
 registerService<PostService>({
     environment: 'local',
     token: PostService,
     useClass: LocalPostService,
     deps: [HttpClient]
-})
-
-export default PostService;
+});
