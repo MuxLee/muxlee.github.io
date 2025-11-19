5b99f9e37962a569cdfc23fa8c2b41ec683ae154
MuxLee <dnfl7080@naver.com>
2025-11-19 23:26:49 +0900

    [feat]: 게시글 화면 추가 - 게시글 화면에대한 `route` 정보 추가 - 게시글 화면 디자인 추가

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from '@page/main/main.component';
+import { PostComponent } from '@page/post/post.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: MainComponent
+    },
+    {
+        path: 'post',
+        loadComponent: () => PostComponent
     }
 ];
