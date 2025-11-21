507f0401d01ac0e3fa838df884da2be552033602
MuxLee <dnfl7080@naver.com>
2025-11-21 22:07:31 +0900

    [fix]: 게시글 메타데이터 변경 및 의존성 초기 설정 - 게시글 메타데이터(`Post`)에 컨텐츠 내용을 반환하는 속성을 추가 - `ngx-markdown` 의존성에서 `markdown` 컴포넌트 사용 시, `HttpClient` 의존성을 통해 바로 `markdown` 파일에 접근하여 불러올 수 있도록 처리

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
+import { provideMarkdown } from 'ngx-markdown';
+
 import { routes } from '@app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
 import { provideService } from '@service/service.provider';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideClientHydration(withEventReplay()),
         provideHttpClient(),
+        provideMarkdown({
+            loader: HttpClient
+        }),
         provideRouter(routes, withComponentInputBinding()),
         provideService(),
         provideZoneChangeDetection({
