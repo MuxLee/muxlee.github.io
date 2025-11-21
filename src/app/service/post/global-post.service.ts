1ac636c0514cdf8e8d3fd9fee441d8927b6eeb0c
MuxLee <dnfl7080@naver.com>
2025-11-21 22:01:12 +0900

    [fix]: 게시글 서비스 기능 수정 - 제네릭 유형 제거(`Post` 유형으로 전면 변경) - 로컬 게시글 서비스의 경우, 게시글 고유값을 `Base64`로 인코딩 된 값을 평문으로 변경하는 로직 추가

--- src/app/service/post/global-post.service.ts
+++ src/app/service/post/global-post.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { type Post } from '@model/post';
 import PostService from '@service/post/post.service';
 
-export class GlobalPostService implements PostService<Post> {
+export class GlobalPostService implements PostService {
 
     getPost(postId: string): Observable<Post> {
         return new Observable(() => {});
--- src/app/service/post/local-post.service.ts
+++ src/app/service/post/local-post.service.ts
@@ -1,20 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import frontMatter from 'front-matter';
+import { map, Observable, tap } from 'rxjs';
 
+import { Post } from '@model/post';
 import PostService from '@service/post/post.service';
 
-export class LocalPostService implements PostService<string> {
+export class LocalPostService implements PostService {
 
-    private httpClient: HttpClient = inject(HttpClient);
+    private httpClient = inject(HttpClient);
 
-    getPost(postFilePath: string): Observable<string> {
+    getPost(postFilePath: string): Observable<Post> {
+        postFilePath = atob(postFilePath);
         postFilePath = postFilePath.replace(/public\//, '');
 
         return this.httpClient.get(`/${postFilePath}`, {
             responseType: 'text'
-        });
+        }).pipe(
+            map(markdown => {
+                const metadata = frontMatter<Post>(markdown).attributes;
+
+                Object.defineProperty(metadata, 'content', {
+                    enumerable: true,
+                    value: markdown
+                });
+
+                return metadata;
+            })
+        );
     }
 
 }
