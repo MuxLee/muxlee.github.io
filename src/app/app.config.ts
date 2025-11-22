0dd88894c1d77219ca2105a346162deaffcd8b93
MuxLee <dnfl7080@naver.com>
2025-11-22 23:58:49 +0900

    [feat & fix]: 기타 추가/변경 사항 - `markdown` 랜더링 관련하여 스타일 의존성 추가(`github-markdown-css`, `katex`, `marked`, `marked-gfm-heading-id`, `prismjs`) - `markdown` 적용을 위해 `app.config.ts` 변경 - 페이지 이동 간의 `transition`이 적용되도록 추가 - 전역 스타일에 컨텐츠 영역에 대한 스타일, `flex` 영역에 대한 `justify` 스타일 추가

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection, SecurityContext } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
-import { provideMarkdown } from 'ngx-markdown';
+import { gfmHeadingId } from 'marked-gfm-heading-id';
+import { MARKED_EXTENSIONS, MARKED_OPTIONS, provideMarkdown, SANITIZE } from 'ngx-markdown';
 
 import { routes } from '@app/app.routes';
 import { provideService } from '@service/service.provider';
@@ -14,9 +14,28 @@ export const appConfig: ApplicationConfig = {
         provideClientHydration(withEventReplay()),
         provideHttpClient(),
         provideMarkdown({
-            loader: HttpClient
+            loader: HttpClient,
+            markedExtensions: [
+                {
+                    provide: MARKED_EXTENSIONS,
+                    useFactory: gfmHeadingId,
+                    multi: true
+                }
+            ],
+            markedOptions: {
+                provide: MARKED_OPTIONS,
+                useValue: {
+                    breaks: true,
+                    gfm: true,
+                    pedantic: false,
+                }
+            },
+            sanitize: {
+                provide: SANITIZE,
+                useValue: SecurityContext.NONE
+            }
         }),
-        provideRouter(routes, withComponentInputBinding()),
+        provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
         provideService(),
         provideZoneChangeDetection({
             eventCoalescing: true
