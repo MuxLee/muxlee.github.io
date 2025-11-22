ceccee39f45b9a0bca6d29aa0a922534f711bacd
MuxLee <dnfl7080@naver.com>
2025-11-22 21:23:50 +0900

    [fix]: `header` 컴포넌트 라우팅 수정 - `main` 화면으로 이동하는 하이퍼링크를 `Angular`의 `routerLink`로 변경₩

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
-    imports: [],
+    imports: [RouterLink],
     selector: 'header-semantic',
     styleUrl: './header.component.css',
     templateUrl: './header.component.html'
